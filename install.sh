fb8d0edc2892131fad0c304527cf652a769e4aa9
ROCHIT <86218252+rxchit@users.noreply.github.com>
2022-07-01 17:47:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ echo "coping dots"
 cp -r dots/* ~/.config
 echo "	"
 
-echo "installing necessary items"
+echo "installing necessary items 'yay should be installed' "
 yay -S bat exa expac find-the-command-git fish-autopair fzf mcfly tealdeer
