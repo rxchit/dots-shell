d6362991c1b9bfbcacdfdb46a35cba7d09073b3c
ROCHIT <rochit56@gmail.com>
2022-07-01 17:35:09 +0530

    config updated

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+echo "coping dots"
 cp -r * ~/.config
+echo "	"
 
-echo("dots copied")
-
+echo "installing necessary items"
 yay -S bat exa expac find-the-command-git fish-autopair fzf mcfly tealdeer
-
-echo("installed necessary items")
